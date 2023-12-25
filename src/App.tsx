541a87ed34c75aac50d9772213b1576b6f10e9a6
Brian Phelan <brianphelan59@gmail.com>
2023-12-25 09:32:22 -0500

    Add types

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ const buttons:Array<string> = ["0","1","2","3","4","5","6","7","8","9"]
 const correctCode:string = "5213" 
 
 function App() {
-  const [passcode, setPasscode] = useState("")
-  const [status, setStatus] = useState("")
+  const [passcode, setPasscode] = useState<string>("")
+  const [status, setStatus] = useState<string>("")
 
   useEffect(() => {
     if(passcode.length >= 4){
