5a1be9e67d06c76765fd33f3a95da281c8cb1f32
Brian Phelan <brianphelan59@gmail.com>
2023-12-25 09:32:38 -0500

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 const buttons:Array<string> = ["0","1","2","3","4","5","6","7","8","9"]
